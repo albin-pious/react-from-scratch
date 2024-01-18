9a4b2dce26780491cbdf633e5273a03a3a88d897
Albin Pious <albinpious65@gmail.com>
2024-01-18 11:36:44 +0530

    added new notes regarding to assignments

--- Workout/app.js
+++ Workout/app.js
@@ -1,16 +1,1 @@
 
-const date = new Date();
-
-const day = date.getDay();
-
-const days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-]
-console.log(days)
-console.log('day is: ',days[day]);
